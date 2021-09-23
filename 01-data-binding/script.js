56cb5f4c283d2bbd732d835fba8607fd02ddeb25
Alex Cabrera <alex.cabrera@gmail.com>
2021-09-23 10:58:14 -0400

    remove answers

--- 01-data-binding/script.js
+++ 01-data-binding/script.js
@@ -1,32 +1,9 @@
 import * as d3 from "d3";
 
 // Q1.1 Select the "text-div" div and set the text to "Hello World".
-let textDiv = document.querySelector("#text-div");
-textDiv.innerHTML = "Hello World";
 
 // Q1.2 Load "beer.csv" and log it to the console.
-d3.csv("../static/beer.csv").then((table) => {
-  console.log(table);
 
-  // Q1.3 List the first 5 beers in the table.
-  let dataDiv = document.querySelector("#data-div");
-  d3.select(dataDiv)
-    .selectAll("p")
-    .data(table.slice(0, 5))
-    .enter()
-    .append("p")
-    .text((d) => d.name);
+// Q1.3 List the first 5 beers in the table.
 
-  // Q1.4 Create simple bars representing the ABV of the first 5 beers.
-  let svgDiv = document.querySelector("#svg-div");
-  d3.select(svgDiv)
-    .append("svg")
-    .selectAll("rect")
-    .data(table.slice(0, 5))
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => i * 45)
-    .attr("y", 0)
-    .attr("width", 50)
-    .attr("height", (d) => d.abv * 1000);
-});
+// Q1.4 Create simple bars representing the ABV of the first 5 beers.
--- 02-scales-axes/script.js
+++ 02-scales-axes/script.js
@@ -1,23 +1,12 @@
 import * as d3 from "d3";
 
 d3.csv("../static/beer.csv").then((table) => {
-  console.log(table);
-
   let barDiv = document.querySelector("#bar-div");
   let padding = 30;
 
   // Q2.1 Create a band (ordinal) scale for the x axis of a bar chart showing ABV of the first 5 beers
-  let xScale = d3
-    .scaleBand()
-    .domain(table.slice(0, 5).map((d) => d.name))
-    .range([0 + padding, barDiv.clientWidth - padding])
-    .paddingInner(0.1);
 
   // Q2.2 Create a linear (quantitative) scale for the y axis of a bar chart showing ABV of the first 5 beers
-  let yScale = d3
-    .scaleLinear()
-    .domain([0, d3.max(table, (d) => d.abv)])
-    .range([500, 0]);
 
   let svg = d3
     .select(barDiv)
@@ -30,17 +19,9 @@ d3.csv("../static/beer.csv").then((table) => {
     .data(table.slice(0, 5))
     .enter()
     .append("rect")
-    .attr("fill", "pink")
-    .attr("x", (d) => xScale(d.name))
-    .attr("y", (d) => 500 - yScale(d.abv))
-    .attr("width", xScale.bandwidth())
-    .attr("height", (d) => yScale(d.abv));
+    .attr("fill", "pink");
 
   // Q2.3 Create and display an x axis
-  let xAxis = d3.axisBottom(xScale);
-  svg.append("g").attr("transform", "translate(0, 500)").call(xAxis);
 
   // Q2.4 Create and display a y axis
-  let yAxis = d3.axisLeft(yScale).tickFormat(d3.format(".0%"));
-  svg.append("g").attr("transform", `translate(${padding}, 0)`).call(yAxis);
 });
