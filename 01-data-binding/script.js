aed4c894786abb294026ad54f84c9388317e2883
Dominik Moritz <domoritz@gmail.com>
2021-09-30 12:13:44 -0400

    Update deps, make page use at most 100% width

--- 01-data-binding/script.js
+++ 01-data-binding/script.js
@@ -2,6 +2,9 @@ import * as d3 from "d3";
 
 // Q1.1 Select the "text-div" div and set the text to "Hello World".
 
+const div = document.querySelector('#text-div')
+div.innerHTML = 'Hello World'
+
 // Q1.2 Load "beer.csv" and log it to the console.
 
 // Q1.3 List the first 5 beers in the table.
